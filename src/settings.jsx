2e0606930798a85d50442104fff12600da850428
MK33-24 <mojahidulislam33mk@gmail.com>
2025-10-17 00:02:07 +0600

    first commit

---
+++ src/settings.jsx
@@ -0,0 +1,4 @@
+export const AppName="MK Download App";
+export const GitHubLink="https://github.com/MK33-24";
+export const PlayStoreLink="https://play.google.com/store/apps?hl=en";
+export const AppStoreLink="https://www.apple.com/app-store/";
